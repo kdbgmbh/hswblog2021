4280990d936cc39e0f53116fdd21838a9056dbad
David Dernovoj <david@dernovoj.com>
2021-04-27 15:38:59 +0200

    Add database, 404 middleware and session route to index

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
 // Blog API
 const express = require('express');
 const err = require('express-error-response');
+const { notFound } = require('express-error-response');
 const app = express();
+
+// Dependencies for the local JSON "database"
+const lowDb = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+// Instantiation of the db adapter and connection
+const adapter = new FileSync('db.json');
+const db = lowDb(adapter);
+
+// Initialize defaults for the database
+db.defaults({
+    blogs: [],
+    maxID: 0,
+    users: [
+        { username: 'mustermann', name: 'Max Mustermann', password: 'password' },
+        { username: 'fritz', name: 'Paula Fritz', password: 'supersecure' },
+    ],
+    sessions: {},
+}).write();
 
 // Register JSON parser for the request body
 app.use(express.json());
@@ -13,6 +33,16 @@ sessionMiddleware(app);
 // Import and register routes
 const blogRoute = require('./routes/blogposts');
 blogRoute(app);
+
+const sessionRoute = require('./routes/session');
+sessionRoute(app);
+
+// Register variables
+app.set('database', db);
+
+app.use(function () {
+    notFound({ error: 'Could not find the route' });
+});
 
 // Register the error middleware to use RequestErrors in the services
 app.use(
